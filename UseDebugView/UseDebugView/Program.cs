94d1752e167add9195fac330898f864d3e4b3f06
soultomind <soultomind@naver.com>
2021-12-06 23:45:34 +0900

    신규 - DebugView 사용하기 개선된 샘플 추가

---
+++ UseDebugView/UseDebugView/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseDebugView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toolkit.UseNowToString = true;
+            Toolkit.DebugWriteLine("Debug!");
+            Toolkit.TraceWriteLine("Trace!");
+
+            Console.WriteLine("Press any key to continue..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UseDebugView/UseDebugView/Toolkit.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace UseDebugView
+{
+    class Toolkit
+    {
+        // DebugView Filter 시 해당 값으로 
+        public static string IncludeFilterName
+        {
+            get { return _sIncludeFilterName; }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _sIncludeFilterName = value;
+                }
+            }
+        }
+        public static string _sIncludeFilterName;
+        public static bool IsDebugEnabled;
+        public static bool IsTraceEnabled;
+        public static bool UseNowToString;
+        static Toolkit()
+        {
+            _sIncludeFilterName = CreateNamespace();
+#if DEBUG
+            IsDebugEnabled = true;
+#endif
+            IsTraceEnabled = true;
+            UseNowToString = true;
+        }
+        private static string CreateNamespace()
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof(Toolkit));
+            return assembly.GetName().Name;
+        }
+
+        private static string NowToString(string format = "yyyy/MM/dd HH:mm:ss")
+        {
+            return DateTime.Now.ToString(format);
+        }
+
+        public static void DebugWriteLine(string message)
+        {
+            if (IsDebugEnabled)
+            {
+                string className = new StackFrame(1).GetMethod().ReflectedType.Name;
+                string methodName = new StackFrame(1, true).GetMethod().Name;
+                if (UseNowToString)
+                {
+                    message = String.Format("[{0}] [{1}.{2}] {3} DEBUG - {4}", 
+                        _sIncludeFilterName, className, methodName, NowToString(), message);
+                }
+                else
+                {
+                    message = String.Format("[{0}] [{1}.{2}] DEBUG - {3}", _sIncludeFilterName, className, methodName, message);
+                }
+                
+                Debug.WriteLine(message);
+            }
+        }
+        public static void TraceWriteLine(string message)
+        {
+            if (IsTraceEnabled)
+            {
+                string className = new StackFrame(1).GetMethod().ReflectedType.Name;
+                string methodName = new StackFrame(1, true).GetMethod().Name;
+                if (UseNowToString)
+                {
+                    message = String.Format("[{0}] [{1}.{2}] {3} DEBUG - {4}",
+                        _sIncludeFilterName, className, methodName, NowToString(), message);
+                }
+                else
+                {
+                    message = String.Format("[{0}] [{1}.{2}] DEBUG - {3}", _sIncludeFilterName, className, methodName, message);
+                }
+                Trace.WriteLine(message);
+            }
+        }
+        
+    }
+}
