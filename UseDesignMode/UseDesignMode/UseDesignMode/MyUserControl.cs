75172b44851a9165c0e80347381d78d7fcc1e6e5
soultomind <soultomind@naver.com>
2021-12-31 01:01:45 +0900

    신규 - WinForm 디자인모드 팁

---
+++ UseDesignMode/UseDesignMode/UseDesignMode/MyUserControl.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace UseDesignMode
+{
+    public partial class MyUserControl : UserControl
+    {
+        public MyUserControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            if (!DesignMode)
+            {
+                // 디버그실행(F5), 실행(Ctrl + F5)
+                Toolkit.TraceWriteLine("Execute");
+            }
+
+            // 디자인모드시 EntryAssembly NULL
+            if (Assembly.GetEntryAssembly() == null)
+            {
+                // 해당 케이스는 프로젝트 컴파일시에 호출되어 짐
+                Toolkit.TraceWriteLine("DesignMode=" + DesignMode);
+                string executingAssemblyLocation = Assembly.GetExecutingAssembly().Location;
+                Toolkit.TraceWriteLine("Assembly.GetExecutingAssembly().Location=" + executingAssemblyLocation);
+                string executablePath = Application.ExecutablePath;
+                Toolkit.TraceWriteLine("Application.ExecutablePath=" + executablePath);
+            }
+            else
+            {
+                string entryAssemblyLocation = Assembly.GetEntryAssembly().Location;
+                Toolkit.TraceWriteLine("Assembly.GetEntryAssembly().Location=" + entryAssemblyLocation);
+            }
+        }
+
+        // 1. DesignerSerializationVisibility Attribute
+        // MainForm.Designer.cs 파일에 컨트롤이 추가되면서 속성 설정코드가 추가되지 않음
+        // [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        // [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+
+        // MainForm.Designer.cs 파일에 컨트롤이 추가되면서 속성 설정코드가 추가됨
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+
+
+        // 2. Browsable Attribute
+        // [Browsable(false)] // 해당 속성을 해당 컨트롤을 사용하는 폼 디자인모드에 표시 하지 않음
+        [Browsable(true)] // 해당 속성을 해당 컨트롤을 사용하는 폼 디자인모드에 표시함
+
+        // 3. Displayname Attribute
+        // 폼 디자인모드시 속성 창에 표시되는 속성명
+        [DisplayName("UseCallback")]
+
+        // 4. Description Attribute
+        // 폼 디자인모드시 속성 창에 표시되는 설명을 지정(속성, 이벤트)
+        [Description("콜백 여부를 지정합니다.")]
+
+        // 5. Category Attribute
+        // 폼 디자인모드시 속성 창에 표시되는 속성이 위치 해 있는 카테고리
+        [Category("MyUserControl")]
+        public bool UseCallback { get; set; } = false;
+    }
+}
---
+++ UseDesignMode/UseDesignMode/UseDesignMode/Toolkit.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseDesignMode
+{
+    internal class Toolkit
+    {
+        /// <summary>
+        /// DebugView Filter 이름
+        /// <para>Filter/Hightlight 메뉴 Include 항목에 사용될 값</para>
+        /// </summary>
+        public static string IncludeFilterName
+        {
+            get { return _sIncludeFilterName; }
+            set
+            {
+                if (!String.IsNullOrEmpty(value) && value.Length > 1)
+                {
+                    _sIncludeFilterName = value;
+                }
+            }
+        }
+        public static string _sIncludeFilterName;
+
+        /// <summary>
+        /// <see cref="System.Diagnostics.Debug"/>.WriteLine 메서드 출력 여부
+        /// </summary>
+        public static bool IsDebugEnabled;
+
+        /// <summary>
+        /// <see cref="System.Diagnostics.Trace"/>.WriteLine 메서드 출력 여부
+        /// </summary>
+        public static bool IsTraceEnabled;
+
+        /// <summary>
+        /// 메시지 출력시 현재시간 출력 여부
+        /// </summary>
+        public static bool UseNowToString;
+        static Toolkit()
+        {
+
+#if DEBUG
+            _sIncludeFilterName = CreateNamespace();
+            IsDebugEnabled = true;
+#else
+            _sIncludeFilterName = "ApplicationName";
+            IsDebugEnabled = false;
+#endif
+            IsTraceEnabled = true;
+            UseNowToString = false;
+        }
+
+        private static string CreateNamespace()
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof(Toolkit));
+            return assembly.GetName().Name;
+        }
+
+        private static string NowToString(string format = "yyyy/MM/dd HH:mm:ss")
+        {
+            return DateTime.Now.ToString(format);
+        }
+
+        public static void DebugWriteLine(string message)
+        {
+            if (IsDebugEnabled)
+            {
+                string className = new StackFrame(1).GetMethod().ReflectedType.Name;
+                string methodName = new StackFrame(1, true).GetMethod().Name;
+                if (UseNowToString)
+                {
+                    message = String.Format("[{0}] [{1}.{2}] {3} DEBUG - {4}",
+                        _sIncludeFilterName, className, methodName, NowToString(), message);
+                }
+                else
+                {
+                    message = String.Format("[{0}] [{1}.{2}] DEBUG - {3}",
+                        _sIncludeFilterName, className, methodName, message);
+                }
+
+                Debug.WriteLine(message);
+            }
+        }
+        public static void TraceWriteLine(string message)
+        {
+            if (IsTraceEnabled)
+            {
+                string className = new StackFrame(1).GetMethod().ReflectedType.Name;
+                string methodName = new StackFrame(1, true).GetMethod().Name;
+                if (UseNowToString)
+                {
+                    message = String.Format("[{0}] [{1}.{2}] {3} TRACE - {4}",
+                        _sIncludeFilterName, className, methodName, NowToString(), message);
+                }
+                else
+                {
+                    message = String.Format("[{0}] [{1}.{2}] TRACE - {3}",
+                        _sIncludeFilterName, className, methodName, message);
+                }
+                Trace.WriteLine(message);
+            }
+        }
+    }
+}
