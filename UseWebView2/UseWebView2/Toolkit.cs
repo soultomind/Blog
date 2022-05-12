3f95816d8b093f9140b38c290acb25f9e1401c4d
soultomind <soultomind@naver.com>
2022-05-12 09:44:53 +0900

    단순 - LocalMachine 일때 운영체제 비트수로 체크하여 처리

--- UseWebView2/UseWebView2/Toolkit.cs
+++ UseWebView2/UseWebView2/Toolkit.cs
@@ -2,55 +2,196 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UseWebView2
 {
-    public static class Toolkit
+    /// <summary>
+    /// 모듈에서 사용되는 툴킷 클래스
+    /// </summary>
+    internal static class Toolkit
     {
+        /// <summary>
+        /// DebugView Filter 이름
+        /// <para>Filter/Hightlight 메뉴 Include 항목에 사용될 값</para>
+        /// </summary>
+        public static string IncludeFilterName
+        {
+            get { return _sIncludeFilterName; }
+            set
+            {
+                if (!String.IsNullOrEmpty(value) && value.Length > 1)
+                {
+                    _sIncludeFilterName = value;
+                }
+            }
+        }
+        private static string _sIncludeFilterName;
+
+        /// <summary>
+        /// <see cref="Debug.WriteLine(object)"/>,<see cref="Debug.Write(object)"/>
+        /// <para>메서드 출력 여부</para>
+        /// </summary>
         public static bool IsDebugEnabled;
+
+        /// <summary>
+        /// <see cref="Trace.WriteLine(object)"/>,<see cref="Trace.Write(object)"/>
+        /// <para>메서드 출력 여부</para>
+        /// </summary>
         public static bool IsTraceEnabled;
 
+        /// <summary>
+        /// 메시지 출력시 현재시간 출력 여부
+        /// </summary>
+        public static bool UseNowToString;
         static Toolkit()
         {
 
 #if DEBUG
+            _sIncludeFilterName = CreateNamespace();
             IsDebugEnabled = true;
-            IsTraceEnabled = true;
 #else
+            _sIncludeFilterName = "CLIPSOFT";
             IsDebugEnabled = false;
-            IsTraceEnabled = true;
 #endif
-
+            IsTraceEnabled = true;
+            UseNowToString = false;
         }
 
-        public static void DebugWriteLine(string message)
+        private static string CreateNamespace()
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof(Toolkit));
+            return assembly.GetName().Name;
+        }
+
+        private static string NowToString(string format = "yyyy/MM/dd HH:mm:ss")
+        {
+            return DateTime.Now.ToString(format);
+        }
+
+        private static string MakeMessage(string header, string message)
+        {
+            if (UseNowToString)
+            {
+#if DEBUG
+                message = String.Format("[{0}] {1} [{2}] DEBUG - {3}", _sIncludeFilterName, NowToString(), header, message);
+#else
+                message = String.Format("[{0}] {1} [{2}] TRACE - {3}", _sIncludeFilterName, NowToString(), header, message);
+#endif
+            }
+            else
+            {
+#if DEBUG
+                message = String.Format("[{0}] [{1}] DEBUG - {2}", _sIncludeFilterName, header, message);
+#else
+                message = String.Format("[{0}] [{1}] TRACE - {2}", _sIncludeFilterName, header, message);
+#endif
+            }
+            return message;
+        }
+
+        /// <summary>
+        /// <see cref="System.Diagnostics.Debug.WriteLine(object)"/>을 활용하여 메시지를 출력합니다.
+        /// </summary>
+        /// <param name="message"></param>
+        internal static void DebugWriteLine(string message)
         {
             if (IsDebugEnabled)
             {
                 string className = new StackFrame(1).GetMethod().ReflectedType.Name;
                 string methodName = new StackFrame(1, true).GetMethod().Name;
                 string header = String.Format("{0} :: {1}", className, methodName);
-                message = String.Format("[{0}] DEBUG - {1}", header, message);
+                message = MakeMessage(header, message);
                 Debug.WriteLine(message);
             }
         }
 
-        public static void TraceWriteLine(string message)
+        /// <summary>
+        /// <see cref="System.Diagnostics.Debug.WriteLine(object)"/>을 활용하여 예외를 출력합니다.
+        /// </summary>
+        /// <param name="ex"></param>
+        internal static void DebugWriteLine(Exception ex)
+        {
+            string className = new StackFrame(1).GetMethod().ReflectedType.Name;
+            string methodName = new StackFrame(1, true).GetMethod().Name;
+            string header = String.Format("{0} :: {1}", className, methodName);
+
+            string message = MakeMessage(header, ex.Message);
+            DebugWriteLine(message);
+
+            message = MakeMessage(header, ex.StackTrace);
+            DebugWriteLine(message);
+        }
+
+        /// <summary>
+        /// <see cref="System.Diagnostics.Debug.Write(object)"/>을 활용하여 메시지를 출력합니다.
+        /// </summary>
+        /// <param name="message"></param>
+        internal static void DebugWrite(string message)
+        {
+            if (IsDebugEnabled)
+            {
+                string className = new StackFrame(1).GetMethod().ReflectedType.Name;
+                string methodName = new StackFrame(1, true).GetMethod().Name;
+                string header = String.Format("{0} :: {1}", className, methodName);
+                message = MakeMessage(header, message);
+                Debug.Write(message);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="System.Diagnostics.Trace.WriteLine(object)"/>을 활용하여 메시지를 출력합니다.
+        /// </summary>
+        /// <param name="message"></param>
+        internal static void TraceWriteLine(string message)
         {
             if (IsTraceEnabled)
             {
                 string className = new StackFrame(1).GetMethod().ReflectedType.Name;
                 string methodName = new StackFrame(1, true).GetMethod().Name;
                 string header = String.Format("{0} :: {1}", className, methodName);
-                message = String.Format("[{0}] TRACE - {1}", header, message);
+                message = MakeMessage(header, message);
                 Trace.WriteLine(message);
             }
         }
 
-        public static bool IsCurrentProcessAdministrator()
+        /// <summary>
+        /// <see cref="System.Diagnostics.Trace.WriteLine(object)"/>을 활용하여 예외를 출력합니다.
+        /// </summary>
+        /// <param name="ex"></param>
+        internal static void TraceWriteLine(Exception ex)
+        {
+            string className = new StackFrame(1).GetMethod().ReflectedType.Name;
+            string methodName = new StackFrame(1, true).GetMethod().Name;
+            string header = String.Format("{0} :: {1}", className, methodName);
+
+            string message = MakeMessage(header, ex.Message);
+            TraceWriteLine(message);
+
+            message = MakeMessage(header, ex.StackTrace);
+            TraceWriteLine(message);
+        }
+
+        /// <summary>
+        /// <see cref="System.Diagnostics.Trace.Write(object)"/>을 활용하여 메시지를 출력합니다.
+        /// </summary>
+        /// <param name="message"></param>
+        internal static void TraceWrite(string message)
+        {
+            if (IsTraceEnabled)
+            {
+                string className = new StackFrame(1).GetMethod().ReflectedType.Name;
+                string methodName = new StackFrame(1, true).GetMethod().Name;
+                string header = String.Format("{0} :: {1}", className, methodName);
+                message = MakeMessage(header, message);
+                Trace.Write(message);
+            }
+        }
+
+        internal static bool IsCurrentProcessExecuteAdministrator()
         {
             bool flag;
 
--- UseWebView2/UseWebView2/WebView2Util.cs
+++ UseWebView2/UseWebView2/WebView2Util.cs
@@ -10,7 +10,7 @@ namespace UseWebView2
 {
     class WebView2Util
     {
-        public static bool IsProcessX64 = IntPtr.Size == 8;
+        public static bool Is64BitOperatingSystem = Environment.Is64BitOperatingSystem;
         public static readonly string ClientsSubId = "{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}";
 
         public static bool InstalledAvailableWebView2Runtime(string browserExecutableFolder = null)
@@ -36,7 +36,7 @@ namespace UseWebView2
         }
         public static bool InstalledRegistryEdgeWebView2Runtime()
         {
-            if (Toolkit.IsCurrentProcessAdministrator())
+            if (Toolkit.IsCurrentProcessExecuteAdministrator())
             {
                 Toolkit.TraceWriteLine("관리자 권한으로 체크합니다. LocalMahcine,CurrentUser");
                 return InstalledEdgeWebView2LoaclMachine() || InstalledEdgeWebView2CurrentUser();
@@ -51,42 +51,31 @@ namespace UseWebView2
         public static bool InstalledEdgeWebView2LoaclMachine()
         {
             bool installed = false;
-            if (IsProcessX64)
-            {
-                using (var targetRegistry = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate\Clients\" + ClientsSubId, RegistryKeyPermissionCheck.ReadWriteSubTree))
-                {
-                    installed = (targetRegistry != null);
-                    if (installed)
-                    {
-                        Toolkit.TraceWriteLine("===== Registry Information ======");
-                        foreach (var name in targetRegistry.GetValueNames())
-                        {
-                            object value = targetRegistry.GetValue(name);
-                            string text = String.Format("{0}={1}", name, value);
 
-                            Toolkit.TraceWriteLine(text);
-                        }
-                        Toolkit.TraceWriteLine("===== Registry Information ======");
-                    }
-                }
+            string name = String.Empty;
+            if (Is64BitOperatingSystem)
+            {
+                name = @"SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate\Clients\" + ClientsSubId;
             }
             else
             {
-                using (var targetRegistry = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\EdgeUpdate\Clients\" + ClientsSubId, RegistryKeyPermissionCheck.ReadWriteSubTree))
-                {
-                    installed = (targetRegistry != null);
-                    if (installed)
-                    {
-                        Toolkit.TraceWriteLine("===== Registry Information ======");
-                        foreach (var name in targetRegistry.GetValueNames())
-                        {
-                            object value = targetRegistry.GetValue(name);
-                            string text = String.Format("{0}={1}", name, value);
+                name = @"SOFTWARE\Microsoft\EdgeUpdate\Clients\" + ClientsSubId;
+            }
 
-                            Toolkit.TraceWriteLine(text);
-                        }
-                        Toolkit.TraceWriteLine("===== Registry Information ======");
+            using (var targetRegistry = Registry.LocalMachine.OpenSubKey(name, RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                installed = (targetRegistry != null);
+                if (installed)
+                {
+                    Toolkit.TraceWriteLine("===== Registry Information ======");
+                    foreach (var valueName in targetRegistry.GetValueNames())
+                    {
+                        object value = targetRegistry.GetValue(valueName);
+                        string text = String.Format("{0}={1}", valueName, value);
+
+                        Toolkit.TraceWriteLine(text);
                     }
+                    Toolkit.TraceWriteLine("===== Registry Information ======");
                 }
             }
             return installed;
